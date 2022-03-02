6525f4399e5bff45cb3fe0e8cb06f71e22cb7b16
Manuj Patel <manuj.p@simformsolutions.com>
2022-03-02 23:16:32 +0530

    created emplyee crud

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,7 @@
 class Employee < ApplicationRecord
+    validates :email, uniqueness: true
+    validates :employee_name, :email, :mobile_number,:birth_date,:password,:gender,:address, presence: true
+    validates :mobile_number, length: {is: 10}
+    validates :birth_date,  comparison: { less_than: Date.today, message: "birthdate can't be in future"}
+    has_one_attached :document
 end
